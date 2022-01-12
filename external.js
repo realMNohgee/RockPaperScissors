d21c8c0b4d63332ac61577b6a7fcce793a4eecef
realMNohgee <clund25@gmail.com>
2022-01-11 18:17:00 -0600

    added the if and else if functions to playRound had it wworking sort of but was kicking out the wrong results for winner so I tried to fix it and now its kicking an undefined error for the consolelog at the end

--- external.js
+++ external.js
@@ -1,12 +1,28 @@
 const computerPlay = (min = 1, max = 4) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
-console.log('1 = Scissors, 2 = Paper, 3 = Rock\n');
+console.log('1 = Scissors, 2 = Paper, 3 = Rock');
+
 
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection === 3 && computerSelection === 1) {
+        return `Player selects ${playerSelection}, Computer selects ${computerSelection}, Player Wins!`
+    } else if (playerSelection === 1 && computerSelection === 3) {
+        return `Player selects ${playerSelection}, Computer selects ${computerSelection}, Computer Wins!`
+    } else if (playerSelection === 2 && computerSelection === 1) {
+        return `Player selects ${playerSelection}, Computer selects ${computerSelection}, Computer Wins!`
+    } else if (playerSelection === 1 && computerSelection === 2) {
+        return  `Player selects ${playerSelection}, Computer selects ${computerSelection}, Player Wins!`
+    } else if (playerSelection === 3 && computerSelection === 2) {
+        return `Player selects ${playerSelection}, Computer selects ${computerSelection}, Player Wins!`
+    } else if (playerSelection === 2 && computerSelection === 3) {
+        return `Player selects ${playerSelection}, Computer selects ${computerSelection}, Computer Wins!`
+    } else if (playerSelection === computerSelection) {
+        return `It's a draw!`
+    }    
 }
+    
 
-const playerSelection = "rock";
+const playerSelection = "Rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
